9f5b49c58057b05d6f0d4c839d4b183a2ad0149c
Jonathan Epp <134516465+eppjonathan@users.noreply.github.com>
2023-09-11 13:35:11 +0200

    11-09-23 edited

--- 11-09-23/interaktives-Formular.js
+++ 11-09-23/interaktives-Formular.js
@@ -2,35 +2,35 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Verhindert das Absenden des Formulars
   
     // Formularfelder abrufen
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var email = document.getElementById("email").value;
-    var birthdate = document.getElementById("birthdate").value;
+    let vorname = document.getElementById("vorname").value;
+    let nachname = document.getElementById("nachname").value;
+    let email = document.getElementById("email").value;
+    let geburtsdatum = document.getElementById("geburtsdatum").value;
   
     // Überprüfen, ob alle Felder ausgefüllt sind
-    if (firstName === "" || lastName === "" || email === "" || birthdate === "") {
+    if (vorname === "" || nachname === "" || email === "" || geburtsdatum === "") {
       alert("Bitte füllen Sie alle Felder aus.");
       return;
     }
   
     // Überprüfen, ob die E-Mail-Adresse gültig ist
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       alert("Bitte geben Sie eine gültige E-Mail-Adresse ein.");
       return;
     }
   
-    // Überprüfen, ob das Geburtsdatum im richtigen Format ist
-    var dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-    if (!dateRegex.test(birthdate)) {
+    // Überprüfen, ob das Geburtsdatum im richtigen Format ist (YYYY-MM-DD)
+    let dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateRegex.test(geburtsdatum)) {
       alert("Bitte geben Sie das Geburtsdatum im Format YYYY-MM-DD ein.");
       return;
     }
   
     // Erfolgsmeldung anzeigen und eingegebene Informationen anzeigen
     alert("Formular erfolgreich abgesendet!");
-    console.log("Vorname: " + firstName);
-    console.log("Nachname: " + lastName);
+    console.log("Vorname: " + vorname);
+    console.log("Nachname: " + nachname);
     console.log("E-Mail: " + email);
-    console.log("Geburtsdatum: " + birthdate);
+    console.log("Geburtsdatum: " + geburtsdatum);
   });
